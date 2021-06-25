5206cabb3583192588e85f2402a4910d3a14fdd4
Atilio <atiliogoulart@gmail.com>
2021-06-25 05:09:11 -0300

    Updating POST student's route

--- app/students/urls.py
+++ app/students/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
 
     path('', views.AllStudents.as_view(), name='all_students'),
+    path('create/', views.CreateStudent.as_view(), name='create_student'),
 
 ]
