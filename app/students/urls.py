ca199c2bc29a85a68a2f2cfef34c73dce2363d5f
Atilio <atiliogoulart@gmail.com>
2021-06-25 05:10:43 -0300

    Updating DELETE student's route

--- app/students/urls.py
+++ app/students/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('', views.AllStudents.as_view(), name='all_students'),
     path('create/', views.CreateStudent.as_view(), name='create_student'),
     path('update/<int:id>/', views.UpdateStudent.as_view(), name='update_student'),
+    path('delete/<int:id>/', views.DestroyStudent.as_view(), name='delete_student'),
 
 ]
