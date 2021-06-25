db703906ba1c6776a0606ea7552c3ddafd43fc85
Atilio <atiliogoulart@gmail.com>
2021-06-25 05:04:36 -0300

    Updating POST student's view

--- app/students/views.py
+++ app/students/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from .models import StudentModel
+from .serializer import StudentSerializer
+
+class AllStudents(generics.ListAPIView):
+    queryset = StudentModel.objects.all()
+    serializer_class = StudentSerializer
