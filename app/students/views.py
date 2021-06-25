f5cb4edc6ab3356b2ba36f86b0c8e9b53aff5900
Atilio <atiliogoulart@gmail.com>
2021-06-25 05:05:40 -0300

    Updating POST student's view

--- app/students/views.py
+++ app/students/views.py
@@ -6,3 +6,7 @@ from .serializer import StudentSerializer
 class AllStudents(generics.ListAPIView):
     queryset = StudentModel.objects.all()
     serializer_class = StudentSerializer
+
+class CreateStudent(generics.CreateAPIView):
+    queryset = StudentModel.objects.all()
+    serializer_class = StudentSerializer
