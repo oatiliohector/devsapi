c9c3e52dbd121ef0ebce2433fd1757f9e7280ec8
Atilio <atiliogoulart@gmail.com>
2021-07-02 22:42:09 -0300

    1.2

--- app/students/views.py
+++ app/students/views.py
@@ -1,23 +1,13 @@
 from rest_framework.response import Response
-from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 
 from .models import StudentModel
 from .serializer import StudentSerializer
 
-@api_view(['GET', 'POST'])
-def snippet_list(request):
-    """
-    List all code snippets, or create a new snippet.
-    """
-    if request.method == 'GET':
-        snippets = StudentModel.objects.all()
-        serializer = StudentSerializer(snippets, many=True)
-        return Response(serializer.data)
+class AllStudents(APIView):
 
-    elif request.method == 'POST':
-        serializer = StudentSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors)
+    def get(self, request):
+        students = StudentModel.objects.all()
+        serializer = StudentSerializer(students, many=True)
+        return Response(serializer.data)
+    
