ad871b6133fa1ab77ff3cc4bfd8ad02014fbdb9c
Atilio <atiliogoulart@gmail.com>
2021-07-02 22:41:02 -0300

    1.1

--- app/students/urls.py
+++ app/students/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from rest_framework.urlpatterns import format_suffix_patterns
+
 from . import views
 
 urlpatterns = [
-    path('', views.snippet_list),
+    path('', views.AllStudents.as_view()),
 ]
 
-urlpatterns = format_suffix_patterns(urlpatterns)
