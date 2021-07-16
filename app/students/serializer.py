37b8d826cf2412e8dd84cbafb53222358dde9aed
Atilio <atiliogoulart@gmail.com>
2021-07-16 17:34:09 -0300

    1.6

---
+++ app/students/serializer.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+
+from .models import StudentModel
+
+class StudentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = StudentModel
+        fields = [
+            'name',
+            'age'
+            ]
