2fd1c9bebe2b061827e30dad3913789492da95c6
Atilio <atiliogoulart@gmail.com>
2021-07-16 17:30:14 -0300

    1.1

--- app/students/models.py
+++ app/students/models.py
@@ -1,9 +1,3 @@
 from django.db import models
 
-class StudentModel(models.Model):
-    name = models.CharField('Name', max_length=255, blank=False)
-    age = models.PositiveIntegerField('Age', default=18, blank=False)
-    
-
-    def __str__(self):
-        return self.name
+# Create your models here.
--- app/students/serializer.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-
-from .models import StudentModel
-
-class StudentSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = StudentModel
-        fields = '__all__'
