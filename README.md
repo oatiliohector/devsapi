bcabbb6663cbda6312ee616c5f5800e7c840d9b8
Atilio <atilio.jesus@grupoboticario.com.br>
2024-09-26 09:17:01 -0300

    chore: 🚧 remove everything

--- README.md
+++
@@ -1,9 +0,0 @@
-# API Sayajin Level 2
-
-
-### Tecnologies:
-
-##### Python
-##### Django
-##### Django Rest Framework
-##### MongoDB (Cloud)
--- app/students/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class StudentModel(models.Model):
-
-    name = models.CharField(max_length=255, blank=False)
-    age = models.IntegerField(default=18, blank=False)
-
-    def __str__(self):
-        return self.name
