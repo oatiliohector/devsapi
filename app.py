3d534912dbdf2a8128d1b1c98465d81b1bc08cd5
Atilio <atilio.jesus@grupoboticario.com.br>
2024-09-28 20:27:21 -0300

    chore: 🚧 add gitignore

--- app.py
+++ app.py
@@ -1,15 +1,12 @@
 from typing import Union
-
 from fastapi import FastAPI
 
 app = FastAPI()
 
-
-@app.get("/")
+@app.get('/api/')
 def read_root():
-    return {"Hello": "World"}
+    return {"Hello World": "Beautiful"}
 
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.get('/items/{item_id}')
+def read_item(item_id: int):
+    return {"item_id": item_id}
